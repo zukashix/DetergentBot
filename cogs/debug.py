f24d99f4412cf659781cbcc1d9ac200614fe81c7
Frank Weaver <67629199+Zukashi2077@users.noreply.github.com>
2021-04-16 11:15:21 +0530

    Removed shutdown command

--- cogs/debug.py
+++ cogs/debug.py
@@ -11,17 +11,11 @@ class Utilities(commands.Cog, name='Utilities'):
     @commands.command(name='about', brief='About the bot.', description='Information on the bot. Type \'~about techinf\' to get technical information.')
     async def about(self, ctx, technef = 'None'):
         await ctx.send(u"Personal bot for Lunoirex#5661 | Made with \u2665 by Zukashi#7071")
+        await ctx.send("By the way, this bot is open-sourced at https://github.com/Zukashi2077/DetergentBot")
+        await ctx.send("If you find any bugs, please create an issue on the GitHub page!")
         if technef.lower() == 'techinf':
             await ctx.send("**Techincal Information:**")
             await ctx.send("```\nPython {}.{}.{}\n---------------\nDiscord.py v{}\n---------------\nLyricGenius v{}\n---------------\nPing: {}ms```".format(pyv.major, pyv.minor, pyv.micro, dcv, lgv, int(self.bot.latency * 1000)))
-
-    @commands.command(name='shutdown', brief='Shutdown the bot.', description='Turns off the bot. Can only be used by an authorized personnel.')
-    async def shutdown(self, ctx):
-        if ctx.author.id != 463657352386707456 or 690400661786984468:
-            await ctx.send('You\'re not authorized for this!')
-        else:
-            await ctx.send('Shutting down . . . /')
-            await self.bot.close()
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
