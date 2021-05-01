56e21bd23a137ae5de88fe9b8d42c702ef5991d4
Frank Weaver <67629199+Zukashi2077@users.noreply.github.com>
2021-05-01 10:35:26 +0530

    Added utilities cog with clear command

---
+++ cogs/utils.py
@@ -0,0 +1,46 @@
+from discord.ext import commands
+from discord.ext.commands import has_permissions, CheckFailure
+from asyncio import sleep 
+
+class Utilities(commands.Cog):
+ """Utilities, mostly moderation"""
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @bot.command(name='clear')
+  @has_permissions(manage_messages=True)
+  async def clear(ctx, amt):
+    try:
+      amt = int(amt)
+    except:
+      await ctx.send('Only numbers allowed as argument!')
+      return
+      
+    if amt > 100:
+      await ctx.send('Only 100 messages at a time dude!!')
+      return
+
+    def check(m):
+      return m.author == ctx.author and m.channel == ctx.channel
+
+    await ctx.send('Are you sure to delete {} messages? (Reply with yes/y or anything else for no)'.format(amt))
+    msgrecv = await bot.wait_for('message', check=check)
+
+    if msgrecv.content.lower() == 'yes' or 'y':
+      await ctx.channel.purge(limit=amt + 3)
+      msgsent = await ctx.send('Cleared {} messages :white_check_mark:'.format(amt))
+      await sleep(3)
+      await msgsent.delete()
+
+    else:
+      msgsent = await ctx.send('Didn\'t touch the messages. *sigh*')
+      await sleep(3)
+      await msgsent.delete()
+
+  @clear.error
+  async def clear_error(error, ctx):
+      if isinstance(error, CheckFailure):
+          await client.send_message(ctx.message.channel, "Looks like you don't have the `manage_messages` permission.")
+
+def setup(bot):
+  bot.add_cog(Utilities(bot))
