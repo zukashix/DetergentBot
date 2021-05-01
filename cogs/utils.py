3d271a411434030e67ebca9a277dcfb92187111d
Frank Weaver <67629199+Zukashi2077@users.noreply.github.com>
2021-05-01 10:40:56 +0530

    Fixed this weird indentation error istg this is horrible

--- cogs/utils.py
+++ cogs/utils.py
@@ -3,44 +3,44 @@ from discord.ext.commands import has_permissions, CheckFailure
 from asyncio import sleep 
 
 class Utilities(commands.Cog):
- """Utilities, mostly moderation"""
-  def __init__(self, bot):
-    self.bot = bot
-    
-  @bot.command(name='clear')
-  @has_permissions(manage_messages=True)
-  async def clear(ctx, amt):
-    try:
-      amt = int(amt)
-    except:
-      await ctx.send('Only numbers allowed as argument!')
-      return
-      
-    if amt > 100:
-      await ctx.send('Only 100 messages at a time dude!!')
-      return
+    """Utilities, mostly moderation"""
+    def __init__(self, bot):
+        self.bot = bot
 
-    def check(m):
-      return m.author == ctx.author and m.channel == ctx.channel
+    @bot.command(name='clear')
+    @has_permissions(manage_messages=True)
+    async def clear(ctx, amt):
+        try:
+            amt = int(amt)
+        except:
+            await ctx.send('Only numbers allowed as argument!')
+            return
 
-    await ctx.send('Are you sure to delete {} messages? (Reply with yes/y or anything else for no)'.format(amt))
-    msgrecv = await bot.wait_for('message', check=check)
+        if amt > 100:
+            await ctx.send('Only 100 messages at a time dude!!')
+            return
 
-    if msgrecv.content.lower() == 'yes' or 'y':
-      await ctx.channel.purge(limit=amt + 3)
-      msgsent = await ctx.send('Cleared {} messages :white_check_mark:'.format(amt))
-      await sleep(3)
-      await msgsent.delete()
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
 
-    else:
-      msgsent = await ctx.send('Didn\'t touch the messages. *sigh*')
-      await sleep(3)
-      await msgsent.delete()
+        await ctx.send('Are you sure to delete {} messages? (Reply with yes/y or anything else for no)'.format(amt))
+        msgrecv = await bot.wait_for('message', check=check)
 
-  @clear.error
-  async def clear_error(error, ctx):
-      if isinstance(error, CheckFailure):
-          await client.send_message(ctx.message.channel, "Looks like you don't have the `manage_messages` permission.")
+        if msgrecv.content.lower() == 'yes' or 'y':
+            await ctx.channel.purge(limit=amt + 3)
+            msgsent = await ctx.send('Cleared {} messages :white_check_mark:'.format(amt))
+            await sleep(3)
+            await msgsent.delete()
+
+        else:
+            msgsent = await ctx.send('Didn\'t touch the messages. *sigh*')
+            await sleep(3)
+            await msgsent.delete()
+
+    @clear.error
+    async def clear_error(error, ctx):
+        if isinstance(error, CheckFailure):
+            await client.send_message(ctx.message.channel, "Looks like you don't have the `manage_messages` permission.")
 
 def setup(bot):
-  bot.add_cog(Utilities(bot))
+    bot.add_cog(Utilities(bot))
